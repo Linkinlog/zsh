b034c4c9ba66ae1c57e9e7b2bba40b6de1c8b360
Linkinlog <dahlton@dahlton.org>
2023-04-20 11:20:02 -0400

    Refac: Change to using functions and local variables
    Refactors to use a function so we can fix a bug with the variable order
    and specify the variables as a local variable and unset the command
    after.

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -2,18 +2,10 @@
 
 # Check for updates to our Github repo
 
-# Args to specify bare repo
-git_args=("--git-dir=$HOME/.dotfiles.git/" "--work-tree=$HOME")
-# TODO make this not hardcoded
-REPO="Linkinlog/.dotfiles"
-# Allows us to be on any branch
-CURRENT_BRANCH=$(config rev-parse --abbrev-ref HEAD)
-# Local/remote commit shows what point we are at in the git history for each
-LOCAL_COMMIT=$(config rev-parse HEAD)
-REMOTE_COMMIT=$(curl --connect-timeout 2 -fsSL -H 'Accept: application/vnd.github.v3.sha' "https://api.github.com/repos/$REPO/commits/$CURRENT_BRANCH")
-
 # General git usage for config
 config() {
+    # Args to specify bare repo
+    local git_args=("--git-dir=$HOME/.dotfiles.git/" "--work-tree=$HOME")
     if [[ $1 == "nvim" ]] || [[ $1 == "tmux" ]] || [[ $1 == "zsh" ]]; then
         local config_dir="$HOME/.config/$1"
         if [[ -d $config_dir ]]; then
@@ -35,15 +27,31 @@ lazyconf() {
     command lazygit "${git_args[@]}" "$@"
 }
 
-# If the most recent remote commit isnt the commit we are on, assume we update
-if [[ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]]; then
-    printf "There is an update available for the repository:\n"
-    printf "Local commit: %s\n" "$LOCAL_COMMIT"
-    printf "Remote commit: %s\n" "$REMOTE_COMMIT"
-    printf "Do you want to update now? (y/n): "
-    read -r answer
-    printf "\n"
-    if [[ "$answer" == "y" ]]; then
-        config pull --quiet --rebase origin "$CURRENT_BRANCH"
+repo_update_check() {
+    local current_branch
+    local local_commit
+    local remote_commit
+    local repo
+    # Allows us to be on any branch
+    current_branch=$(config rev-parse --abbrev-ref HEAD)
+    # Local/remote commit shows what point we are at in the git history for each
+    repo="Linkinlog/.dotfiles" # TODO make this not hardcoded
+    local_commit=$(config rev-parse HEAD)
+    remote_commit=$(curl --connect-timeout 2 -fsSL -H 'Accept: application/vnd.github.v3.sha' "https://api.github.com/repos/$repo/commits/$current_branch")
+
+    # If the most recent remote commit isnt the commit we are on, assume we update
+    if [[ "$local_commit" != "$remote_commit" ]]; then
+        printf "There is an update available for the repository:\n"
+        printf "Local commit: %s\n" "$local_commit"
+        printf "Remote commit: %s\n" "$remote_commit"
+        printf "Do you want to update now? (y/n): "
+        read -r answer
+        printf "\n"
+        if [[ "$answer" == "y" ]]; then
+            config pull --quiet --rebase origin "$current_branch"
+        fi
     fi
-fi
+}
+
+repo_update_check
+unset repo_update_check
