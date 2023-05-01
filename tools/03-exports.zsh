0b7a72ec5a966c7360507a2e614a5e54116dbfca
Linkinlog <dahlton@dahlton.org>
2023-05-01 08:21:20 -0400

    Revert "fix(formatting): Better formatting in commands.zsh"
    This reverts commit 2da320a8d47c376a137db1be5d14aed5ec2da013.

--- tools/03-exports.zsh
+++ tools/03-exports.zsh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
 
 # If you come from bash you might have to change your $PATH.
+export GOPATH=$HOME/go
 export GOROOT=/usr/local/go
-export GOBIN=$GOROOT/bin
+export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
+export PATH=/usr/local/go/bin:$PATH
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
